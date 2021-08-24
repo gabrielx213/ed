b1bbfdfcf7c14cb3e3cf48d46409b8213007017c
Horusprg <newconnect7@hotmail.com>
2021-08-24 14:10:40 -0300

    Grupo
    Alunos:
    GABRIEL XAVIER VIEIRA DO NASCIMENTO -202007040030
    ANDRE VINICIUS NEVES ALVES - 202007040035
    HAYLA LUIZA DA COSTA ALMEIDA - 202006840023
    FLAVIO RAFAEL TRINDADE MOURA - 202006840019

--- arranjo/Arranjo.java
+++ arranjo/Arranjo.java
@@ -1,4 +1,5 @@
 package arranjo;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Arranjo<T> {
 
@@ -16,7 +17,9 @@ public class Arranjo<T> {
     public void Set(int i, T obj) {
         this.Data[i] = obj;
     }
-
+    public int Tam(){
+        return this.Data.length;
+    }
     public void Imprime() {
         System.out.print("[");
         for (int i=0; i < this.Data.length; i++) {
@@ -24,5 +27,63 @@ public class Arranjo<T> {
         }
         System.out.println("]");
     }
+    
 
+    public void a(T obj) {
+        if (this.Get(this.Tam()-1) == null){
+            this.Set(this.Tam()-1, obj);
+        }
+        this.Imprime();
+    }
+    
+    public void b(T obj) {
+        for (int i=this.Tam()-1; i > 0; i--){
+            this.Set(i, this.Get(i-1)); 
+        }
+        this.Imprime();
+        this.Set(0, obj);
+        this.Imprime();
+    }
+
+    public void c(T obj){
+        int aux = ThreadLocalRandom.current().nextInt(0, this.Tam());
+        for (int i=this.Tam()-1; i > aux; i--){
+            this.Set(i, this.Get(i-1)); 
+        }
+        this.Imprime();
+        this.Set(aux, obj);
+        this.Imprime();
+    }
+
+    public void d(T obj){
+        this.Set(this.Tam()-1, obj);
+        this.Imprime();
+        Object ultimo = this.Get(this.Tam()-1);
+        this.Set(this.Tam()-1, null);
+        this.Imprime();
+        System.out.println("O último dado era: " + ultimo);
+    }
+
+    public void e(){
+        Object primeiro = this.Get(0);
+        this.Set(0, null);
+        this.Imprime();
+        for (int i=0; i < this.Tam()-1; i++){
+            this.Set(i, this.Get(i+1));
+        }
+        this.Imprime();
+        System.out.println("O primeiro dado era: " + primeiro);
+    }
+
+    public void f(){
+        int aux = ThreadLocalRandom.current().nextInt(0, this.Tam()-1); 
+        Object dado = this.Get(aux);
+        this.Set(aux, null);
+        this.Imprime();
+        for (int i=aux; i < this.Tam()-1; i++){
+            this.Set(i, this.Get(i+1));
+        }
+        this.Imprime();
+        System.out.println("O dado retirado foi: " + dado);
+    }
 }
--- arranjo/TesteArranjo.java
+++ arranjo/TesteArranjo.java
@@ -1,16 +1,59 @@
 package arranjo;
+import java.util.concurrent.TimeUnit;
 
 public class TesteArranjo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 		Arranjo<String> arr1 = new Arranjo<String>(5);
-		
-		arr1.Set(0,"a");
-        arr1.Set(1,"b");
-        System.out.println(arr1.Get(1));
-        arr1.Set(2,"c");
+        Arranjo<String> arr2 = new Arranjo<String>(5);
+        Arranjo<String> arr3 = new Arranjo<String>(5);
+        Arranjo<String> arr4 = new Arranjo<String>(5);
+        Arranjo<String> arr5 = new Arranjo<String>(5);
+        Arranjo<String> arr6 = new Arranjo<String>(5);
+        for(int i = 0; i<4; i++){
+            arr1.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+            arr2.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+            arr3.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+            arr4.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+            arr5.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+            arr6.Set(i, String.valueOf(RandomCharacter.getRandomLowerCaseLetter()));
+        }
         arr1.Imprime();
-		arr1.Set(3,"d");
+        arr2.Imprime();
+        arr3.Imprime();
+        arr4.Imprime();
+        arr5.Imprime();
+        arr6.Imprime();
+        System.out.println("Operando com as alternativas para cada arranjo:\n\n\n\n\n");
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra a */
+        System.out.println("\nLetra a:");
         arr1.Imprime();
+        arr1.a("n");
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra b */
+        System.out.println("\nLetra b:");
+        arr2.Imprime();
+        arr2.b("o");
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra c */
+        System.out.println("\nLetra c:");
+        arr3.Imprime();
+        arr3.c("p");
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra d */
+        System.out.println("\nLetra d:");
+        arr4.Imprime();
+        arr4.d("q");
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra e */
+        System.out.println("\nLetra e:");
+        arr5.Imprime();
+        arr5.e();
+        TimeUnit.SECONDS.sleep(3);
+        /** Letra f */
+        System.out.println("\nLetra f:");
+        arr6.Imprime();
+        arr6.f();
 	}   
 }
